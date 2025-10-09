b5bcd1a4fe7de75e8cf44e7355d22d438f6beb34
eduardo895 <poisola7@gmail.com>
2025-10-09 17:31:11 +0100

    Adicionado função para atualizar produto, listar produtos com stock baixo(que precisam ser repostos) e listar os n produtos mais vendidos.

--- produtos.py
+++ produtos.py
@@ -15,6 +15,38 @@ def adicionar_produto():
     }).execute()
     print("Produto adicionado")
 
+def atualizar_produto():
+    produto_id = int(input("Qual o ID do produto: "))
+    nome = input("Novo nome: ")
+    plataforma = input("Nova plataforma: ")
+    preco = float(input("Novo preço: "))
+    stock = int(input("Novo stock: "))
+    descricao = input("Nova descrição: ")
+    supabase.table("produtos").update({
+        "nome" : nome,
+        "plataforma" : plataforma,
+        "preco" : preco,
+        "stock" : stock,
+        "descricao" : descricao
+    }).eq("id", produto_id).execute()
+    print("Produto atualizado")
+
+def listar_produtos_com_stock_baixo():
+    response = (
+    supabase.table("produtos").select('nome','stock').lt('stock',3).execute()
+    )
+    print(response.data)
+
+def lista_produtos_mais_vendidos():
+    response = (
+        supabase.table("produtos").select('nome','vendas').limit(3).order('vendas', desc=True).execute()
+    )
+    print(response.data)
+
+def remover_produto(produto_id):
+    #Código por fazer
+    print("Produto removido com sucesso.")
+    
 def listar_produtos():
     response = supabase.table("produtos").select("*").execute()
     print(response.data)
@@ -22,6 +54,9 @@ def listar_produtos():
 print("Funções: ")
 print("1 -> Adicionar produtos")
 print("2 -> Listar produtos")
+print("3 -> Editar produtos")
+print("4 -> Listar produtos que precisam ser repostos")
+print("5 -> Listar os 3 produtos mais vendidos")
 funcao = int(input("Que função quer fazer na tabela produtos(insira o número): "))
 match funcao:
         case 1:
@@ -29,3 +64,12 @@ match funcao:
         
         case 2:
           listar_produtos()
+
+        case 3:
+          atualizar_produto()
+        
+        case 4:
+          listar_produtos_com_stock_baixo()
+        
+        case 5:
+          lista_produtos_mais_vendidos()
