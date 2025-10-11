ed265e9a8986070ab20f0c3b9ed8b73c50b9af02
eduardo895 <poisola7@gmail.com>
2025-10-11 11:26:21 +0100

    Adicionado a lógica da compra, também foi implementado uma verificação de tipo de utilizador antes do menu de compra.

--- compras.py
+++ compras.py
@@ -1,9 +1,50 @@
 from db import supabase
 from tabulate import tabulate
+import getpass
+import bcrypt
+from datetime import datetime
 
-def fazer_compra():
+def fazer_login_cliente():
+    email = input("Email utilizador: ")
+    password = getpass.getpass("Password: ")
+
+    res = supabase.table("clientes").select("id", "password").eq("email", email).execute()
+    if not res.data:
+        print("Cliente não encontrado.")
+        return None
+
+    user = res.data[0]
+    hashed = user["password"]
+
+    if bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8')):
+        print("Login bem-sucedido, pode continuar para o menu de compras.")
+        return user["id"]
+    else:
+        print("Password incorreta.")
+        return None  
+    
+def fazer_login_admin():
+    email = input("Email do admin: ")
+    password = getpass.getpass("Password: ")
+
+    res = supabase.table("admins").select("id", "password").eq("email", email).execute()
+    if not res.data:
+        print("Admin não encontrado.")
+        return None
+
+    admin = res.data[0]
+    hashed = admin["password"]
+
+    if bcrypt.checkpw(password.encode('utf-8'), hashed.encode('utf-8')):
+        print("Login bem-sucedido.")
+        return admin["id"]
+    else:
+        print("Password incorreta.")
+        return None
+
+def fazer_compra(user_id):
     print("Produtos disponíveis:\n")
-    response = supabase.table("produtos").select('id', 'nome', 'preco', 'stock', 'plataforma').execute()
+    response = supabase.table("produtos").select('id', 'nome', 'preco', 'stock', 'plataforma', 'vendas').execute()
 
     if response.data:
         produtos = response.data
@@ -23,9 +64,29 @@ def fazer_compra():
                 confirmar = input("\nTem certeza(S/N): ")
                 confirmar_maiusculo = confirmar.upper()
                 if confirmar_maiusculo == "S":
-                    print("Continuar a lógica da compra")
+                    print("Opção escolhida: ", confirmar_maiusculo)
+                    if produto_escolhido["stock"] > 0:
+                        novo_stock = produto_escolhido["stock"] - 1
+                        vendas_novas = produto_escolhido["vendas"] + 1
+                        supabase.table("produtos").update({
+                            "stock": novo_stock,
+                            "vendas": vendas_novas
+                        }).eq("id", produto_escolhido["id"]).execute()
+
+                        supabase.table("compras").insert({
+                            "cliente_id": user_id,
+                            "produto_id": produto_escolhido["id"],
+                            "data": datetime.now().isoformat()
+                        }).execute()
+
+                        print("\nCompra feita com sucesso!!")
+
+                    else:
+                        print("Produto sem stock disponível.")
+
                 elif confirmar_maiusculo == "N":
-                    print("Continuar a lógica da compra")
+                    print("Opção escolhida", confirmar_maiusculo)
+                    print("\nCompra cancelada")
                 else:
                     print("Opção inválida!!")
             else:
@@ -35,6 +96,35 @@ def fazer_compra():
     else:
         print("Nenhum produto encontrado!")
 
+def listar_compras():
+    print("Por fazer")
 
+def listar_compras_por_cliente():
+    print("Por fazer")
 
-fazer_compra()
+print("Fazer login para aceder o menu compras: \n")
+
+print("Escolha o tipo de utilizador:")
+print("1 -> Cliente")
+print("2 -> Administrador")
+tipo = int(input("Opção: "))
+
+if tipo == 1:
+    user_id = fazer_login_cliente()
+    if user_id:
+        fazer_compra(user_id)
+elif tipo == 2:
+    admin_id = fazer_login_admin()
+    if admin_id:
+        print("Que tipo de listagem vai fazer: \n")
+        print("1 -> Listagem de todas as compras")
+        print("2 -> Listagem de compras de um x cliente")
+        escolha = int(input("Opção: "))
+        if escolha == 1:
+            listar_compras()
+        elif escolha == 2:
+            listar_compras_por_cliente()
+        else:
+            print("Opção inválida!!")
+else: 
+    print("Opção inválida!!")
