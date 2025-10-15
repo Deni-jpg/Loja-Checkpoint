2789c33b1b39da720b889e63b76119b9ef2e0511
Eduardo Simões <123998309+eduardo895@users.noreply.github.com>
2025-10-15 14:50:28 +0000

    Adicionado a opção de pesquisar jogos por nome/plataforma.

--- compras.py
+++ compras.py
@@ -43,58 +43,86 @@ def fazer_login_admin():
         return None
 
 def fazer_compra(user_id):
-    print("Produtos disponíveis:\n")
-    response = supabase.table("produtos").select('id', 'nome', 'preco', 'stock', 'plataforma', 'vendas').execute()
 
-    if response.data:
-        produtos = response.data
-        for i, produto in enumerate(produtos, start=1):
-            print(f"{i}. {produto['nome']} ({produto['plataforma']}) - {produto['preco']:.2f}€ | Stock: {produto['stock']}")
-
-        # Escolha do utilizador
-        escolha = input("\nDigite o número do produto que deseja comprar: ")
-        try:
-            escolha = int(escolha)
-            if 1 <= escolha <= len(produtos):
-                produto_escolhido = produtos[escolha - 1]
-                print(f"\nNome: {produto_escolhido['nome']} ({produto_escolhido['plataforma']})")
-                print(f"Preço: €{produto_escolhido['preco']:.2f}")
-                print(f"Stock disponível: {produto_escolhido['stock']}")
-                #Confirmar
-                confirmar = input("\nTem certeza(S/N): ")
-                confirmar_maiusculo = confirmar.upper()
-                if confirmar_maiusculo == "S":
-                    print("Opção escolhida: ", confirmar_maiusculo)
-                    if produto_escolhido["stock"] > 0:
-                        novo_stock = produto_escolhido["stock"] - 1
-                        vendas_novas = produto_escolhido["vendas"] + 1
-                        supabase.table("produtos").update({
-                            "stock": novo_stock,
-                            "vendas": vendas_novas
-                        }).eq("id", produto_escolhido["id"]).execute()
-
-                        supabase.table("compras").insert({
-                            "cliente_id": user_id,
-                            "produto_id": produto_escolhido["id"],
-                            "data": datetime.now().isoformat()
-                        }).execute()
-
-                        print("\nCompra feita com sucesso!!")
-
-                    else:
-                        print("Produto sem stock disponível.")
-
-                elif confirmar_maiusculo == "N":
-                    print("Opção escolhida", confirmar_maiusculo)
-                    print("\nCompra cancelada")
-                else:
-                    print("Opção inválida!!")
+    escolha2 = int(input("\nPrefere pesquisar o nome/plataforma do jogo ou ver todos os produtos disponíveis e selecionar através desse menu? (1 -> pesquisar; 2 -> ver lista produtos) \n"))
+    if escolha2 == 1:
+        print(" -- Menu Procurar -- ")
+        escolha3 = int(input("\nProcurar por nome ou plataforma? (1 --> nome, 2 --> plataforma)"))
+        if escolha3 == 1:
+            nome_jogo = input("Insira o nome do jogo: ")
+            response = supabase.table("produtos").select("*").ilike("nome", f"%{nome_jogo}%").execute()
+            print("\nJogos: ", "\n")
+            if response.data:
+                produtos = response.data
+                for i, produto in enumerate(produtos, start=1):
+                    print(f"{i}. {produto['nome']} ({produto['plataforma']}) - {produto['preco']:.2f}€ | Stock: {produto['stock']}")
             else:
-                print("Número inválido.")
-        except ValueError:
-            print("Entrada inválida. Digite um número.")
-    else:
-        print("Nenhum produto encontrado!")
+                print("Jogo não encontrado!!")
+        elif escolha3 == 2:
+            plataforma_jogo = input("Insira a plataforma: ")
+            response = supabase.table("produtos").select("*").ilike("plataforma",f"%{plataforma_jogo}%").execute()
+            print("\nJogos da plataforma: ", plataforma_jogo, "\n")
+            if response.data:
+                produtos = response.data
+                for i, produto in enumerate(produtos, start=1):
+                    print(f"{i}. {produto['nome']} ({produto['plataforma']}) - {produto['preco']:.2f}€ | Stock: {produto['stock']}")
+            else:
+                print("Nenhum jogo encontrado nessa plataforma!!")
+    elif escolha2 == 2:    
+        print("Produtos disponíveis:\n")
+        response = supabase.table("produtos").select('id', 'nome', 'preco', 'stock', 'plataforma', 'vendas').execute()
+
+        if response.data:
+            produtos = response.data
+            for i, produto in enumerate(produtos, start=1):
+                print(f"{i}. {produto['nome']} ({produto['plataforma']}) - {produto['preco']:.2f}€ | Stock: {produto['stock']}")
+
+            # Escolha do utilizador
+            escolha = input("\nDigite o número do produto que deseja comprar: ")
+            try:
+                escolha = int(escolha)
+                if 1 <= escolha <= len(produtos):
+                    produto_escolhido = produtos[escolha - 1]
+                    print(f"\nNome: {produto_escolhido['nome']} ({produto_escolhido['plataforma']})")
+                    print(f"Preço: €{produto_escolhido['preco']:.2f}")
+                    print(f"Stock disponível: {produto_escolhido['stock']}")
+                    #Confirmar
+                    confirmar = input("\nTem certeza(S/N): ")
+                    confirmar_maiusculo = confirmar.upper()
+                    if confirmar_maiusculo == "S":
+                        print("Opção escolhida: ", confirmar_maiusculo)
+                        if produto_escolhido["stock"] > 0:
+                            novo_stock = produto_escolhido["stock"] - 1
+                            vendas_novas = produto_escolhido["vendas"] + 1
+                            supabase.table("produtos").update({
+                                "stock": novo_stock,
+                                "vendas": vendas_novas
+                            }).eq("id", produto_escolhido["id"]).execute()
+
+                            supabase.table("compras").insert({
+                                "cliente_id": user_id,
+                                "produto_id": produto_escolhido["id"],
+                                "data": datetime.now().isoformat()
+                            }).execute()
+
+                            print("\nCompra feita com sucesso!!")
+
+                        else:
+                            print("Produto sem stock disponível.")
+
+                    elif confirmar_maiusculo == "N":
+                        print("Opção escolhida", confirmar_maiusculo)
+                        print("\nCompra cancelada")
+                    else:
+                        print("Opção inválida!!")
+                else:
+                    print("Número inválido.")
+            except ValueError:
+                print("Entrada inválida. Digite um número.")
+        else:
+            print("Nenhum produto encontrado!")
+    else: 
+        print("\nOpção Inválida")
 
 def listar_compras():
     print("Por fazer")
