83a568ea6926e1f28ad7405cfad54a6e845a361f
rodrigo14052005 <rodrigocolaco31@gmail.com>
2025-10-22 12:59:26 +0000

    Refactor: Atualizado o codigo sql.

--- db.sql
+++ db.sql
@@ -1,38 +1,33 @@
 -- WARNING: This schema is for context only and is not meant to be run.
 -- Table order and constraints may not be valid for execution.
 
-CREATE TABLE public.admins (
-  id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  nome text NOT NULL,
-  email text NOT NULL UNIQUE,
-  password text NOT NULL,
-  criado_em timestamp without time zone DEFAULT CURRENT_TIMESTAMP,
-  CONSTRAINT admins_pkey PRIMARY KEY (id)
-);
-CREATE TABLE public.clientes (
-  id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  nome text NOT NULL,
-  email text NOT NULL UNIQUE,
-  password text NOT NULL,
-  criado_em timestamp without time zone DEFAULT now(),
-  CONSTRAINT clientes_pkey PRIMARY KEY (id)
-);
 CREATE TABLE public.comentarios (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
   produto_id bigint,
-  autor text,
+  user_id uuid,
   texto text,
   aprovado boolean DEFAULT true,
   CONSTRAINT comentarios_pkey PRIMARY KEY (id),
-  CONSTRAINT comentarios_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id)
+  CONSTRAINT comentarios_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id),
+  CONSTRAINT comentarios_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id)
 );
 CREATE TABLE public.compras (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  cliente_id bigint,
+  user_id uuid,
   produto_id bigint,
   data timestamp without time zone DEFAULT now(),
   CONSTRAINT compras_pkey PRIMARY KEY (id),
+  CONSTRAINT compras_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id),
   CONSTRAINT compras_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id)
+);
+CREATE TABLE public.perfil (
+  id uuid NOT NULL DEFAULT gen_random_uuid(),
+  user_id uuid,
+  nome text NOT NULL,
+  tipo text NOT NULL CHECK (tipo = ANY (ARRAY['cliente'::text, 'admin'::text])),
+  criado_em timestamp with time zone DEFAULT CURRENT_TIMESTAMP,
+  CONSTRAINT perfil_pkey PRIMARY KEY (id),
+  CONSTRAINT perfil_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id)
 );
 CREATE TABLE public.produtos (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
@@ -46,9 +41,10 @@ CREATE TABLE public.produtos (
 );
 CREATE TABLE public.wishlist (
   id bigint GENERATED ALWAYS AS IDENTITY NOT NULL,
-  cliente_id bigint,
+  user_id uuid,
   produto_id bigint,
   adicionado_em timestamp without time zone DEFAULT now(),
   CONSTRAINT wishlist_pkey PRIMARY KEY (id),
+  CONSTRAINT wishlist_user_id_fkey FOREIGN KEY (user_id) REFERENCES auth.users(id),
   CONSTRAINT wishlist_produto_id_fkey FOREIGN KEY (produto_id) REFERENCES public.produtos(id)
 );
